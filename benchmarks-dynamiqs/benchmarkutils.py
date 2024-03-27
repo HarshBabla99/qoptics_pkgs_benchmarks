24c011a8bf85a7e921be129dfaf8803b034b485e
HarshBabla99 <harshbabla@gmail.com>
2024-03-27 12:07:50 -0400

    dynamiqs - init commit (copied qutip files)

---
+++ benchmarks-dynamiqs/benchmarkutils.py
@@ -0,0 +1,46 @@
+import timeit
+import json
+import dynamiqs as dq
+import jax.numpy as jnp
+from os.path import exists
+
+benchmark_directory = "benchmarks-QuTiP"
+commitID = qutip.version.version
+result_path = "../results/results-QuTiP-{}-{}.json"
+
+def examplename(name):
+    if name.endswith("]"):
+        return name.rsplit("[", 1)[0]
+    else:
+        return name
+
+
+def run_benchmark(f, *args, samples=5, evals=1):
+    D = {"f": f, "args": args}
+    t = timeit.repeat("f(*args)", globals=D, number=evals, repeat=samples)
+    return min(t)/evals
+
+
+def check(name, D, eps=1e-5):
+    check_path = "../checks/" + examplename(name) + ".json"
+    
+    if exists(check_path):
+        print("\t Checking against check file.")
+
+        f = open(check_path)
+        data = json.load(f)
+        for (N, result) in D.items():
+            r = data[str(N)]
+            if np.isnan(result) or abs(result-r)/abs(r) > eps:
+                print("Warning: Result may be incorrect in", name, ": ", result, "<->", r)
+
+    else:
+        print("\t No check file found - write results to check file.")
+        f = open(check_path, "w")
+        json.dump(D, f)
+        f.close()
+
+def save(name, results):
+    f = open(result_path.format(commitID, name), "w")
+    json.dump(results, f)
+    f.close()
