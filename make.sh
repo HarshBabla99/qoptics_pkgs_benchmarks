9f0e36bda9601c6224e8ff3bae618ce7eeb4448b
HarshBabla99 <harshbabla@gmail.com>
2024-04-04 22:56:12 -0400

    made a bash file

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=dynamiqs-benchmarking
+#SBATCH --output=dynamiqs-benchmarking.log 
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --mem=5G
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=harsh.babla@yale.edu
+
+module load miniconda
+conda activate qi_cuda
+
+python run_all.py
+python collect_results.py
+python plot_results.py
