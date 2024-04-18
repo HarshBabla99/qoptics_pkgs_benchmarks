e45161810d7212d7411557f5cb27f318dc35c582
HarshBabla99 <HarshBabla@gmail.com>
2024-04-18 16:29:00 -0400

    clear.sh updated

--- clear.sh
+++ clear.sh
@@ -1,5 +1,9 @@
-rm plots/*.png
+# rm plots/*.png
 rm out/results/*.json
-rm out/results-collected/*.json
+# rm out/results-collected/*.json
 rm out/checks/*.json
 # rm out/sourcecode/*
+
+rm src/benchmarks-dynamiqs/__pycache__/*
+rm src/benchmarks-QuTiP/__pycache__/*
+rm __pycache__/*
